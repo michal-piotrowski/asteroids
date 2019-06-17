50dbcd39f20d3c0edbeeae5328363653416da917
Michał Piotrowski <Mi.Piotrowski@post.com>
2019-06-17 12:29:21 +0200

    - randomly placed brick shaped asteroids

--- js/Asteroids.js
+++ js/Asteroids.js
@@ -13,42 +13,55 @@ const viewAngle = 45;
 const nearClipping = 0.1;
 const farClipping = 9999;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(viewAngle, width / height, nearClipping, farClipping);
+const camera = new THREE.PerspectiveCamera(viewAngle, width / height, nearClipping, farClipping)
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setClearColor(0x000308);
 renderer.setSize(width, height);
-renderer.
+// renderer.
 document.body.appendChild(renderer.domElement);
 
 window.addEventListener('resize', () => {
-    THREE.Frustum
+    // THREE.Frustum
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 });
+/********************** LESS BORING GAME SETTINGS ******************/
+const SHOOT_INTERVAL = 400;                   // in ms
+const PROJECTILE_TTL = 2000;                  // Time to live in ms
+const METEORITE_SPEED = 0.2;                  // delta of z of meteorite position
+const METEORITE_SPAWN_INTERVAL = 1000;        // in ms
+const METEORITE_TTL = 1000;
 
 /************************** INITIALIZE OBJECTS *************************/
 
 let projectiles = [];
-
-// let meteorites = [
-//     new Meteorite(-2, 4, -3, 2, 1, 1, M_TYPE.SPHERE),
-//     new Meteorite(-2, -12, -3, 1, 1, 1, M_TYPE.SPHERE, {color: 0x1fef23}),
-//     new Meteorite(-1, 0, -6, 1, 1, 1, M_TYPE.SPHERE, {color: 0xff1fa3}),
-//     new Meteorite(1, 0, -9, 1, 1, 1, M_TYPE.SPHERE, {color: 0x2e02f3})
-// ];
+let meteorites = [];
 
 let shipGeometry = new THREE.ConeGeometry(0.5, 1, 4);
 let shipMaterial = new THREE.MeshLambertMaterial({color: 0x00ff00});
 let ship = new THREE.Mesh(shipGeometry, shipMaterial);
-let shipBox = new THREE.BoxHelper(ship, 0xffff00); // w celach wykrywania kolizji, ale
-ship.add(shipBox);
-// scene.add(shipBox);
+let shipBBox = new THREE.Box3().setFromObject(ship);
 
+
+/************************* TO REMOVE ******************** */
 let cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 let cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
 let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+let cubeBox = new THREE.BoxHelper(cube, 0xfafa00);
+cube.add(cubeBox);
+/** */
+let cubeBBox = new THREE.Box3().setFromObject(cube);
+
+ship.canShoot = true;
+let canAddMeteorite = true;
+let shipBox = new THREE.BoxHelper(ship, 0xffff00); // w celach wykrywania kolizji, ale
+
+
+ship.add(shipBox);
+
+// scene.add(shipBox);
 
 
 let sphereGeometry = new THREE.SphereGeometry(0.5, 8, 8);
@@ -90,7 +103,7 @@ scene.add(sphere);
 // light.position.z = 0;
 // scene.add(light);
 
-let light = new THREE.HemisphereLight(0xaa00dd, 0xffffff, 0.5);
+let light = new THREE.HemisphereLight(0xaa00dd, 0xffffff, 0.75);
 light.position.x = 0;
 light.position.y = 10;
 light.position.z = 0;
@@ -121,7 +134,7 @@ scene.fog = new THREE.FogExp2( 0x000000, 0.06 );
 /************************* MOUSE CONTROLS **************************/
 const controls = new THREE.FlyControls(camera);
 
-document.addEventListener('onfocus', () => {
+document.addEventListener('onfocus', (event) => {
     controls.movementSpeed = 0.005;
     controls.domElement = renderer.domElement;
     controls.rollSpeed = Math.PI / 24;
@@ -147,46 +160,100 @@ document.addEventListener("keydown", onDocumentKeyDown, false);
  w pętli update: bullet.position.add(bullet.velocity)
  */
 const sendProjectile = function() {
-    let projectileGeometry = new THREE.SphereGeometry(0.1, 6, 6);
-    let projectileMaterial = new THREE.MeshLambertMaterial({color: 0xFDA50F});
-    let projectile = new THREE.Mesh(projectileGeometry, projectileMaterial);
+    if (ship.canShoot) {
+        let projectileGeometry = new THREE.SphereGeometry(0.1, 6, 6);
+        let projectileMaterial = new THREE.MeshLambertMaterial({color: 0xFDA50F, fog: false});
+        let projectile = new THREE.Mesh(projectileGeometry, projectileMaterial);
 
-    scene.add(projectile);
+        scene.add(projectile);
 
-    let shipPos = ship.getWorldPosition(new THREE.Vector3());
+        let shipPos = ship.getWorldPosition(new THREE.Vector3());
 
-    projectile.position.set(shipPos.x, shipPos.y, shipPos.z);
-    projectile.velocity = new THREE.Vector3(
-        -Math.sin(camera.rotation.y) * Math.cos(camera.rotation.x),
-        Math.sin(camera.rotation.x),
-        -Math.cos(camera.rotation.y)
-    );
-    DevUtils.logVector(projectile.velocity, '');
-    DevUtils.logVector(camera.rotation, 'CAMERA: ');
-    projectiles.push(projectile);
+        projectile.position.set(shipPos.x, shipPos.y, shipPos.z);
+        projectile.velocity = new THREE.Vector3(
+            -Math.sin(camera.rotation.y) * Math.cos(camera.rotation.x),
+            Math.sin(camera.rotation.x),
+            -Math.cos(camera.rotation.y)
+        );
 
-    var origin = new THREE.Vector3( 0, 0, 0 );
-    var length = 1;
-    var hex = 0xffff00;
+        let cubeBox = new THREE.BoxHelper(cube, 0xfafa00);
+        cube.add(cubeBox);
 
-    var arrowHelper = new THREE.ArrowHelper( projectile.velocity, origin, length, hex );
-    ship.add( arrowHelper );
+        projectile.geometry.computeBoundingBox();
+        projectile.bBox = new THREE.Box3().setFromObject(projectile);
 
+        projectile.alive = true;
+        setTimeout(() => {
+            projectile.alive = false;
+            scene.remove(projectile);
+            projectiles.unshift(projectile);
+        }, PROJECTILE_TTL);
 
+        ship.canShoot = false;
+        setTimeout(() => {
+            ship.canShoot = true;
+        }, SHOOT_INTERVAL);
+
+        DevUtils.logVector(projectile.velocity, '');
+        DevUtils.logVector(camera.rotation, 'CAMERA: ');
+        projectiles.push(projectile);
+
+        var origin = new THREE.Vector3( 0, 0, 0 );
+        var length = 1;
+        var hex = 0xffff00;
+
+        var arrowHelper = new THREE.ArrowHelper( projectile.velocity, origin, length, hex );
+        ship.add( arrowHelper );
+    }
 };
-
+//
 function onDocumentKeyDown(event) {
+    event.preventDefault();
     let keyCode = event.which;
     if (keyCode === 32) { //spacebar
         sendProjectile();
     }
-};
+}
 
 
 const accelerateProjectiles = function() {
     for (let p of projectiles)
     {
         p.position.add(p.velocity);
+        if (p.bBox.intersectsBox(cubeBBox)) {
+            console.log('ship intersecting cube');
+        }
+
+    }
+};
+
+const addRandomMeteorite = function() {
+    if (canAddMeteorite) {
+        let meteoriteGeometry = new THREE.BoxGeometry(THREE.Math.randFloat(0.5, 2), THREE.Math.randFloat(0.5, 2), THREE.Math.randFloat(0.5, 2));
+        let meteoriteMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
+        let meteorite = new THREE.Mesh(meteoriteGeometry, meteoriteMaterial);
+        meteorite.position.set(THREE.Math.randFloat(-10, 10), THREE.Math.randFloat(-2, 2), -20);
+        meteorite.acceleration = THREE.Math.randFloat(-0.06, 0.09);
+
+
+        canAddMeteorite = false;
+        setTimeout(() => {
+            canAddMeteorite = true;
+        }, METEORITE_SPAWN_INTERVAL);
+
+        meteorites.push(meteorite);
+        scene.add(meteorite);
+
+        setTimeout(() => {
+            canAddMeteorite = true;
+        }, METEORITE_TTL);
+    }
+};
+
+const accelerateMeteorites = function() {
+    for (let m of meteorites)
+    {
+        m.position.z += METEORITE_SPEED + m.acceleration;
     }
 };
 
@@ -197,6 +264,8 @@ camera.add(ship);
 scene.add(camera);
 
 ship.rotation.x = Math.PI * (-0.3);
+ship.geometry.computeBoundingBox();
+cube.geometry.computeBoundingBox();
 
 function animate() {
 
@@ -206,11 +275,10 @@ function animate() {
 
     controls.update(delta);
 
-    // if (ship.intersectsObject(cube)) {
-    //     console.log('ship intersecting cube');
-    // }
-
+    addRandomMeteorite();
     accelerateProjectiles();
+    accelerateMeteorites();
+
     ship.position.set(0,-1.2,-5);
 
     // update();
